8fafec448c24dde96e55341671cc3fdd555686b4
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2023-02-06 20:23:47 +0530

    contact schema added in sanity

--- backend_sanity/schemas/contact.js
+++ backend_sanity/schemas/contact.js
@@ -1,3 +1,22 @@
 export default {
-    
+    name: 'contact',
+    type: 'document',
+    title: 'Contact',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+        {
+            name: 'email',
+            title: 'Email',
+            type: 'string'
+        },
+        {
+            name: 'message',
+            title: 'Message',
+            type: 'text'
+        }
+    ]
 }
