c39949277c7980498c9356ce0cc05759b09308de
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2023-02-06 20:32:41 +0530

    skills data template added in sanity

--- backend_sanity/schemas/skills.js
+++ backend_sanity/schemas/skills.js
@@ -1,3 +1,25 @@
 export default {
-    
+    name: 'skills',
+    type: 'document',
+    title: 'Skills',
+    field: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+        {
+            name: 'bgColor',
+            title: 'BgColor',
+            type: 'string'
+        },
+        {
+            name: 'icon',
+            title: 'Icon',
+            type: 'image',
+            options: {
+                hotspot: true,
+            },
+        },
+    ]
 }
