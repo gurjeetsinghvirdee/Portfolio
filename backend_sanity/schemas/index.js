bf1ae3d5629fe6b4f0736c9c63ccd000ee4cfdd4
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2023-02-06 19:00:35 +0530

    more schemas added to sanity

--- backend_sanity/schemas/index.js
+++ backend_sanity/schemas/index.js
@@ -1,3 +1,18 @@
+import work from './works';
 import testimonials from "./testimonials";
+import about from './about';
+import experiences from './experience';
+import skills from './skills';
+import workExperience from './workExperience'
+import contact from './contact'
 
-export const schemaTypes = [testimonials]
+
+export const schemaTypes = [
+    work, 
+    testimonials, 
+    about, 
+    experiences,
+    skills,
+    workExperience,
+    contact
+]
