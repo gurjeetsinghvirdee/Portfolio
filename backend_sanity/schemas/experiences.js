c4fdfdf1dfbf4ddffb7988d1cfca140637733193
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2023-02-06 20:29:30 +0530

    experiences template added in sanity

--- backend_sanity/schemas/experiences.js
+++ backend_sanity/schemas/experiences.js
@@ -1,5 +1,18 @@
 export default {
     name: 'experiences',
     type: 'document',
-    title: 'Experiences'
+    title: 'Experiences',
+    field: [
+        {
+            name: 'year',
+            title: 'Year',
+            type: 'string'
+        },
+        {
+            name: 'works',
+            title: 'Works',
+            type: 'array',
+            of: [{type: 'workExperience'}]
+        },
+    ]
 }
