3e37630bb6c82ecff69fa9d5c6601f898c523d3a
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2023-02-07 23:04:57 +0530

    Navbar exported from components to App.js

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { About, Footer, Header, Skills, Testimonials, Work } from './container';
+import { Navbar } from './components';
 
 const App = () => {
   return (
     <div className='App'>
+        <Navbar />
         <Header />
         <About />
         <Work />
