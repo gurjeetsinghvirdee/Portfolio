dd659b515a161a4a4c92cf1962aa735aff5e844d
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2023-02-09 00:51:31 +0530

    created webpack.config.js for sass loader

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { About, Footer, Header, Skills, Testimonials, Work } from './container';
 import { Navbar } from './components';
-import './Sass/App.scss';  // Sass not working have to fix this issue!
+import './App.scss';  // Sass not working have to fix this issue!
 
 const App = () => {
   return (
